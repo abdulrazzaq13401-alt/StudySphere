dabc7b80bd60085618349eaddda40d60fd897e89
Abdul Razzaq <abdulrazzaq13401@gmail.com>
2026-01-25 12:34:15 +0500

    Create Study Sphere Api

---
+++ StudySphere.API/Controllers/CoursesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudySphere.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        string[] course =
+        [
+            "Programming fundamentafls",
+            "Object oriented programming",
+            "Data structures and algorithms",
+            "Database management systems",
+            "Web development",
+            "Mobile app development",
+            "Cloud computing",
+            "Cybersecurity basics",
+            "Software testing and quality assurance",
+            "DevOps practices"
+        ];
+
+        // GET api/courses
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return course;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return course[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value) { }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
---
+++ StudySphere.API/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.EnvironmentName == "Development")
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
